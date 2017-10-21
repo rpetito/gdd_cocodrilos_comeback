598988f50978ca6e6671dc547ae0018082a2a797
nicogaray <nicolas.jm.garay@gmail.com>
2017-10-21 18:00:01 -0300

    agregada la conexion a la base

---
+++ PagoAgilFrba/BaseDeDatos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba
+{ 
+        class BaseDeDatos
+        {
+            public static object BD_Conexion { get; set; }
+
+            public static string ObtenerCadenaConexion()
+            {
+                return Properties.Settings.Default.GD2C2017ConnectionStringTest;
+            }
+
+            public static SqlConnection ObternerConexion()
+            {
+                SqlConnection Con = new SqlConnection(ObtenerCadenaConexion());
+                Con.Open();
+                return Con;
+
+            }
+        }
+}
+
