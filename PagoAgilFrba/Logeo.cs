7af0d22277a7d4ec5b71fed36c5629bd1b46e905
MauroMowa <mowa_mauro95@hotmail.com>
2017-11-20 00:00:08 -0300

    logeo visual logica

--- PagoAgilFrba/Logeo.cs
+++ PagoAgilFrba/Logeo.cs
@@ -23,8 +23,84 @@ namespace PagoAgilFrba
 
         private void Ingresar_Click(object sender, EventArgs e)
         {
-            Menu menu = new PagoAgilFrba.Menu();
-            menu.Show();
+            String pUsername = UsuarioTB.Text;
+            String pContrasenia = ContraseniaTB.Text;
+
+            try
+            {
+                SqlConnection Conexion = BaseDeDatos.ObternerConexion();
+                MessageBox.Show("estamos conectados");
+                SqlCommand loginUsuario = new SqlCommand();
+                SqlDataReader result;
+
+                using (loginUsuario = new SqlCommand("COCODRILOS_COMEBACK.INGRESAR_USUARIO", Conexion))
+                {
+                    loginUsuario.CommandType = CommandType.StoredProcedure;
+                    loginUsuario.Parameters.Add("@username", SqlDbType.Char);
+                    loginUsuario.Parameters["@username"].Value = pUsername;
+                    loginUsuario.Parameters.Add("@password", SqlDbType.Char);
+                    loginUsuario.Parameters["@password"].Value = pContrasenia;
+                }
+
+                result = loginUsuario.ExecuteReader();
+
+                Boolean success = true;
+
+                while (result.Read())
+                {
+                    if (result.VisibleFieldCount == 3)
+                    {
+                        Usuario.getInstance().setDNI(result.GetDecimal(0));
+
+                        Rol rol = new Rol();
+                        rol.setID(result.GetInt32(1));
+                        rol.setDetalle(result.GetString(2));
+
+                        Usuario.getInstance().addRol(rol);
+
+                    }
+                    else
+                    {
+                        if (result.GetInt32(0) == 0)
+                            MessageBox.Show("Usuario inhabilitado", "hola");
+                        else
+                            MessageBox.Show("Usuario o Contraseña incorrecto/s");
+
+                        success = false;
+                    }
+
+
+                }
+
+                if (success)
+                {
+                    if (Usuario.getInstance().getRoles().Count == 1)
+                    {
+                        Usuario.getInstance().setRolSeleccionado(Usuario.getInstance().getRoles()[0]);
+                        Menu menu = new Menu();
+                        menu.ShowDialog();
+                    }
+                    else if (Usuario.getInstance().getRoles().Count > 0)
+                    {
+                        SeleccionRol rolSeleccionado = new SeleccionRol();
+                        rolSeleccionado.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("El usuario no tiene roles disponibles");
+                    }
+                }
+
+                Conexion.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "there was an issue!");
+
+            }
+        
         }
     }
 }
